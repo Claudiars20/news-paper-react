fdb6a25ad6929d2797cb478eef3b3ad58eb39498
Claudia Rojas <60309104+Claudiars20@users.noreply.github.com>
2021-10-18 22:35:56 -0500

    Update News-paper.js

--- src/components/News-paper.js
+++ src/components/News-paper.js
@@ -49,82 +49,83 @@ const useStyles = makeStyles((theme) => ({
 const General = {
   title:
     "Después de 17 meses de pandemia, Lima no registra personas fallecidas por COVID-19",
-  image:"https://estaticos-cdn.elperiodico.com/clip/d84320a1-f65a-41b7-b42e-cf65afbdeac6_alta-libre-aspect-ratio_default_0.jpg",
-  enlace: "https://gestion.pe/peru/coronavirus-lima-metropolitana-no-registro-muertes-por-covid-19-el-ultimo-lunes-nndc-noticia/",
+  image:"https://imag.malavida.com/mvimgbig/download-fs/blockman-go-23598-3.jpg",
+  enlace: "",
 };
 
 const Politica = [
   {
     title:
-      "Encuesta Ipsos: El 61% considera que el presidente Pedro Castillo no tiene liderazgo. Además, el 83% indica que Vladimir Cerrón debe alejarse del Gobierno. En Lima esta cifra crece al 90%.",
-    image: "https://e.rpp-noticias.io/normal/2021/09/29/001100_1150522.jpg",
-    enlace: "https://t.co/6Yh4n49bdJ",
-  },
-  {
-    title:
-      "Aníbal Torres sobre extradición de Alejandro Toledo: “Recibimos con satisfacción el fallo que respalda la posición del Estado peruano”",
+      "Anahí Durand sobre renegociar gas de Camisea: “No tengamos miedo de debatir esos temas”  La ministra de la Mujer aseveró que una renegociación de las utilidades del gas de Camisea debe darse en el “marco de la institucionalización debida”",
     image:
-      "https://larepublica.pe/resizer/vDL14jFzIqoCkvSNIkz_i6uZvAY=/1200x660/top/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/E6FVBY3UYFBU3FHDBZJWBQCHUA.jpg",
-    enlace: "https://t.co/9ElcCz58CV",
+      "https://larepublica.pe/resizer/TbRmpET8mXmKn_wcgIIbBBcUPIY=/1200x660/top/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/RD6H3JQNRRECLOCW4HGBISXRDY.png",
+    enlace: "https://t.co/g87LEaxrBJ",
   },
   {
     title:
       "No fue uno, ni dos, ni tres, ni cuatro, sino fueron 15 videos en la cuenta oficial de Twitter. Ministerio de Trabajo usa sus redes sociales para defender a Iber Maraví a pocos horas de la interpelación en el Congreso.",
     image:
-      "https://e.rpp-noticias.io/xlarge/2021/09/29/125312_1150920.jpg",
+      "https://larepublica.pe/resizer/k0H1nexcktt5Vd9_QsB0qa7RmAA=/538x0/top/larepublica.pe/resizer/y4Xfazs_iF1ykp0MOIXKm56f4kk=/538x0/top/smart/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/SZGR4B7VWZEHJJJPWFDXDUFB7A.png",
     enlace:
       "https://t.co/5kh3SmfKxH",
+  },
+  {
+    title:
+      "Perú Libre: tres congresistas marcan asistencia, pese a que una tiene orden de captura y dos recolectan firmas en Huancayo.",
+    image: "https://www.expreso.com.pe/wp-content/uploads/2021/09/cONGRESISTA-DE-PERU-LIBRE-1.jpg",
+    enlace: "https://t.co/6Yh4n49bdJ",
   },
 ];
 
 const Deporte = [
   {
     title:
-      "¡LOS VICTORIANOS! ClubALoficial  es elegido como el mejor equipo de la fecha 13. El cuadro dirigido por Carlos Bustos  venció a alianzasullana_ y lidera la tabla de la Fase 2  y del acumulado. ¡Bien jugado!",
+      "LOS VICTORIANOS! ClubALoficial  es elegido como el mejor equipo de la fecha 13. El cuadro dirigido por Carlos Bustos  venció a alianzasullana_ y lidera la tabla de la Fase 2  y del acumulado. ¡Bien jugado!",
     image:"https://pbs.twimg.com/media/E9RzNfwXEAE5Kmm.jpg",
-    enlace: "https://t.co/8gNR7bXnlP",
-  },
-  {
-    title:"¡Cristiano Ronaldo, una leyenda en la Champions League!",
-    image:"https://media.gettyimages.com/photos/cristiano-ronaldo-of-manchester-united-poses-with-the-trophy-his-picture-id81190931",
-    enlace: "https://t.co/YnFK4WdJ4r",
+    enlace: "https://t.co/DCGUgbdBG1",
   },
   {
     title:"¡LA META! Tras la victoria ante el Real Madrid, Gustavo Dulanto, defensa del Sheriff Tiraspol, deja en claro que el objetivo del plantel es acceder a los octavos de final de la Champions League.",
     image: "http://www.huaral.pe/wp-content/uploads/2021/09/gustavo-dulanto-el-lider-peruano-del-sheriff-que-hace-historia-y-suena-en-grande.jpg",
-    enlace: "https://t.co/FwK4BIB6i7",
+    enlace: "https://t.co/8gNR7bXnlP",
   },
+  {
+    title:"¿Estamos ante el PEOR Barcelona de los últimos tiempos? 3-0 vs Bayern  3-0 vs Benfica  0 puntos  0 goles a favor  6 goles en contra  1 tiro al arco en 2 partidos de UCL",
+    image:"https://pbs.twimg.com/media/FAexv54XIAsAbCc.jpg",
+    enlace: "https://t.co/YnFK4WdJ4r",
+  },
+  
 ];
 
 const Farandula = [
   {
     title:
-      "Mariella Zanetti arremete contra Sofía Franco: “Ni un hijo debe ser motivo para continuar una relación",
+      "Juan Víctor se cansó que Andrea San Martín y Sebastián expongan a su hija: “Por su integridad mental”",
     image:
-      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOXX8Q.img?h=630&w=1200&m=6&q=60&o=t&l=f&f=jpg&x=493&y=201",
-    enlace: "https://t.co/OPI5mBwZK6",
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOYF5y.img?h=0&w=600&m=6&q=60&u=t&o=f&l=f&x=557&y=279",
+    enlace: "https://t.co/KitPmulj0N",
   },
   {
     title:
-      "Juan Víctor se cansó que Andrea San Martín y Sebastián expongan a su hija: “Por su integridad mental”",
+      "Mariella Zanetti arremete contra Sofía Franco: “Ni un hijo debe ser motivo para continuar una relación",
     image:
-      "https://larepublica.pe/resizer/hstSH3hcRKLCw2gHGkBkevhvoYo=/1200x660/top/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/46FR235GSVBY5A4EPA7KH7UHFU.png",
-    enlace: "https://t.co/KitPmulj0N",
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOXX8Q.img?h=630&w=1200&m=6&q=60&o=t&l=f&f=jpg&x=493&y=201",
+    enlace: "https://t.co/ioC4WjnEfO",
   },
 ];
 
 const Mundial = [
   {
-    title:"La necesitamos. La abuela de MiyaMarcano, la joven que desapareció en Florida, dice que la familia tiene mucha fe de que la van a encontrar. Es duro para nosotros, agregó Violeta Delville.",
-    image:"https://as01.epimg.net/colombia/imagenes/2021/10/03/actualidad/1633295155_691969_1633295474_noticia_normal_recorte1.jpg",
-    enlace: "https://t.co/TygZPDpY6U",
-  },
-  {
     title:
     "Momento en que la lava cae al mar. La colada de lava del volcán de La Palma, en las islas Canarias, llegó en la noche del martes al océano Atlántico. Por el momento, los gases tóxicos provocados por el contacto con el agua representan un riesgo bajo para la población.",
     image:
-    "https://images.clarin.com/2021/09/29/una-nuebe-blanca-ascieden-desde___nGHXmWeNq_720x0__1.jpg",
+    "https://fotografias.lasexta.com/clipping/cmsimages02/2021/09/29/2F2C80DE-F4F1-4384-8E90-E645B6640B39/98.jpg?crop=1250,703,x0,y116&width=1900&height=1069&optimize=low&format=webply",
     enlace: "https://t.co/nLBzOx7OmE",
+  },
+  {
+    title:"Varios ensayos clínicos han mostrado que la combinación de vacunas de covid-19 no sólo es posible, sino que en muchos casos es hasta recomendable y podría ofrecer una mejor protección contra el coronavirus y, particularmente, contra las nuevas variantes.",
+    image:"https://ichef.bbci.co.uk/news/640/cpsprodpb/184AB/production/_119899499_1.jpg",
+    enlace: "https://t.co/7TMLWosRrV",
   },
 ];
 
